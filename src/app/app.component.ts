245af5011b05d3635ad65921ad65d609493b1696
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-09 18:50:58 +0200

    route-outlet on #appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>AppComponent Works</h1>
+    <router-outlet></router-outlet>
   `,
 })
 export class AppComponent {}
