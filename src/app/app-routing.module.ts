7a305c9bf958677496a92db0d3158c07feeec80f
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-09 18:49:12 +0200

    added route to app skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { SkeletonComponent } from './skeleton/skeleton.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', component: SkeletonComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
