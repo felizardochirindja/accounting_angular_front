72cc025047f1f6f2aa3d457e30edb8261d70f4b6
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-10 13:52:12 +0200

    moved page-not-found component to pages folder

--- src/app/pages/error/page-not-found/page-not-found.component.ts
+++ src/app/pages/error/page-not-found/page-not-found.component.ts
@@ -1,9 +1,13 @@
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
+  imports: [
+    RouterModule,
+  ],
   styles: [
   ]
 })
