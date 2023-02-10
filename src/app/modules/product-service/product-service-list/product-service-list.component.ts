69cc58f5645bf673f1d626666aa82ef287c09710
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-10 13:54:59 +0200

    created product-service component

---
+++ src/app/modules/product-service/product-service-list/product-service-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-service-list',
+  templateUrl: './product-service-list.component.html',
+  styles: [
+  ]
+})
+export class ProductServiceListComponent {
+
+}
