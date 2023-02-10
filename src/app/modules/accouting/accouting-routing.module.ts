9202f3810f12c3e787e3e4aacac9de246bcadfb4
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-10 14:02:04 +0200

    added more app routes

--- src/app/modules/accouting/accouting-routing.module.ts
+++ src/app/modules/accouting/accouting-routing.module.ts
@@ -1,11 +1,14 @@
+import { ProductServiceListComponent } from './../product-service/product-service-list/product-service-list.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
 import { AccountListComponent } from './account/account-list/account-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'plano-de-contas', component: AccountListComponent,
-  },
+  { path: '', redirectTo: '/error/page-not-found', pathMatch: 'full' },
+  { path: 'plano-de-contas', component: AccountListComponent },
+  { path: 'classes', component: CategoryListComponent },
+  { path: 'productos-servicos', component: ProductServiceListComponent }
 ];
 
 @NgModule({
