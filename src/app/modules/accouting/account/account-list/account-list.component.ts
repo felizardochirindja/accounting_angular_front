2aeeb3462289da66b5161e25a4a8a42f57ba4c01
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-10 08:45:22 +0200

    created accouting list component

---
+++ src/app/modules/accouting/account/account-list/account-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-list',
+  templateUrl: './account-list.component.html',
+})
+export class AccountListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
