6d1944623dfb3a2d25e4452c15a2b91b0f925560
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-11 22:13:12 +0200

    created authentication service

---
+++ src/app/core/authentication/authentication.service.ts
@@ -0,0 +1,41 @@
+import { Client } from './../client/client.types';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  public isAuthenticated: boolean = false;
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  set accessToken(token: string) {
+    localStorage.setItem('accessToken', token);
+  }
+
+  get accessToken(): string {
+    return localStorage.getItem('accessToken') ?? '';
+  }
+
+  signUp(client: Client): Observable<any> {
+    return this.httpClient.post('api/auth/sign-up', client);
+  }
+
+  signIn() {
+    if (this.isAuthenticated) {
+      return throwError(() => new Error('company is already logged in.'));
+    }
+
+    // implement login logic
+  }
+
+  signOut(): Observable<any> {
+    localStorage.removeItem('accessToken');
+    this.isAuthenticated = false;
+    return of(true);
+  }
+}
