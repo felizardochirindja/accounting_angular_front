541c76f48e0f943cf2dada789791fc13621d0abc
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-12 03:21:19 +0200

    created #getItems() method

--- src/app/modules/product-service/shared/product-service.service.ts
+++ src/app/modules/product-service/shared/product-service.service.ts
@@ -15,7 +15,27 @@ export class ProductServiceService {
     return this.item.asObservable();
   }
 
-  get items$(): Observable<ProductService[]> {
-    return this.items.asObservable();
+  getItems(): Observable<Items[]> {
+    const productsWithoutStock: ProductWithoutStock[] = [
+      { name: 'producto 1', price: 100 },
+      { name: 'producto 2', price: 200 },
+    ];
+  
+    const productsWithStock: ProductWithStock[] = [
+      { name: 'producto 3', price: 300 },
+    ];
+
+    const services: Service[] = [
+      { name: 'servico 1', price: 400 },
+    ];
+
+    const items: Items[] = [
+      ...productsWithStock,
+      ...productsWithoutStock,
+      ...services,
+    ]
+
+    this.itemsSubject.next(items);
+    return of(items);
   }
 }
