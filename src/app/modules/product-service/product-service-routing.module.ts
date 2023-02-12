fa42b36e618f27e506ccf7b15e5d4e3b1ea168bd
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-12 03:19:03 +0200

    resolved items on product-service-list route

--- src/app/modules/product-service/product-service-routing.module.ts
+++ src/app/modules/product-service/product-service-routing.module.ts
@@ -1,9 +1,15 @@
+import { ProductServiceItemsResolver } from './shared/product-service.resolver';
 import { ProductServiceListComponent } from './product-service-list/product-service-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: ProductServiceListComponent }
+  {
+    path: '', component: ProductServiceListComponent,
+    resolve: {
+      items: ProductServiceItemsResolver
+    }
+  },
 ];
 
 @NgModule({
