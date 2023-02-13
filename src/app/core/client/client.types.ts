216d80b1ba6e5cf03ef931a7da6ecebc3bc4cb12
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-13 19:10:57 +0200

    changed nuit type to Nuit model

--- src/app/core/client/client.types.ts
+++ src/app/core/client/client.types.ts
@@ -1,9 +1,12 @@
+import { Nuit } from './../valueObjects/Nuit.model';
 import { Email } from './../valueObjects/Email.model';
 
 export interface Client {
     name?: string;
-    nuit?: string;
+    nuit?: Nuit;
     local?: string;
     email?: Email;
     phone?: string;
 }
+
+export type Supplier = Client;
