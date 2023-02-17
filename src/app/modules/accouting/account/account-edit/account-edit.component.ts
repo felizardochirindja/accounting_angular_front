1eec5bfca5a0b76250f5d9398bd3fc6f1ce08a98
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-17 16:24:44 +0200

    edit account component logic

--- src/app/modules/accouting/account/account-edit/account-edit.component.ts
+++ src/app/modules/accouting/account/account-edit/account-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Account } from './../account.types';
+import { Component, OnInit } from '@angular/core';
+import { AccountingService } from '../../shared/accounting.service';
 
 @Component({
   selector: 'app-account-edit',
@@ -6,6 +9,22 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class AccountEditComponent {
+export class AccountEditComponent implements OnInit {
+  public accounts: Account[] = [];
 
+  constructor(
+    private accountingService: AccountingService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.accountingService.accounts$.subscribe((accounts) => {
+      this.accounts = accounts;
+    })
+  }
+
+  submitAccount() {
+    this.router.navigate(['/inicio/contabilidade/plano-de-contas']);
+  }
 }
