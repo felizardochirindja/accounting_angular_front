f46e7e8038a7a47782ca79457bbc2699ec91cd0a
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-17 21:32:43 +0200

    added id for all accounts

--- src/app/modules/accouting/shared/accounting.service.ts
+++ src/app/modules/accouting/shared/accounting.service.ts
@@ -51,30 +51,28 @@ export class AccountingService {
       {
         name: 'conta mae 1', id: '1', categoryId: '1',
       },
-      { name: 'conta filha 1.1', parentId: '1', categoryId: '1' },
+      { name: 'conta filha 1.1', parentId: '1', categoryId: '1', id: '6' },
       {
-        name: 'conta filha 1.2', parentId: '1', categoryId: '1',
-        children: [
-          { name: 'conta filha 1.2.1', parentId: '1', categoryId: '1' },
-        ],
+        name: 'conta filha 1.2', parentId: '1', categoryId: '1', id: '3',
       },
-      { name: 'conta filha 1.3', parentId: '1', categoryId: '1' },
+      { name: 'conta filha 1.2.1', parentId: '1', categoryId: '1', id: '4' },
+      { name: 'conta filha 1.3', parentId: '1', categoryId: '1', id: '5' },
       {
         name: 'conta mae 2', id: '2', categoryId: '3',
       },
-      { name: 'conta filha 2.1', parentId: '3', categoryId: '3' },
-      { name: 'conta filha 2.2', parentId: '3', categoryId: '3' },
+      { name: 'conta filha 2.1', parentId: '3', categoryId: '3', id: '7' },
+      { name: 'conta filha 2.2', parentId: '3', categoryId: '3', id: '8' },
       {
         name: 'conta mae 3', id: '3', categoryId: '2',
       },
-      { name: 'conta filha 3.1', parentId: '3', categoryId: '2' },
-      { name: 'conta filha 3.2', parentId: '3', categoryId: '2' },
-      { name: 'conta filha 3.3', parentId: '3', categoryId: '2' },
-      { name: 'conta filha 3.3', parentId: '3', categoryId: '2' },
+      { name: 'conta filha 3.1', parentId: '3', categoryId: '2', id: '9' },
+      { name: 'conta filha 3.2', parentId: '3', categoryId: '2', id: '10' },
+      { name: 'conta filha 3.3', parentId: '3', categoryId: '2', id: '11' },
+      { name: 'conta filha 3.3', parentId: '3', categoryId: '2', id: '12' },
       {
         name: 'conta mae 4', id: '4', categoryId: '1',
       },
-      { name: 'conta filha 4.1', parentId: '4', categoryId: '1' },
+      { name: 'conta filha 4.1', parentId: '4', categoryId: '1', id: '13' },
     ];
 
     this.accountsSubject.next(accounts);
@@ -87,36 +85,36 @@ export class AccountingService {
       {
         name: 'conta mae 1', id: '1', categoryId: '1',
         children: [
-          { name: 'conta filha 1.1', parentId: '1', categoryId: '1' },
+          { name: 'conta filha 1.1', parentId: '1', categoryId: '1', id: '6' },
           {
-            name: 'conta filha 1.2', parentId: '1', categoryId: '1',
+            name: 'conta filha 1.2', parentId: '1', categoryId: '1', id: '3',
             children: [
-              { name: 'conta filha 1.2.1', parentId: '1', categoryId: '1' },
+              { name: 'conta filha 1.2.1', parentId: '1', categoryId: '1', id: '4' },
             ],
           },
-          { name: 'conta filha 1.3', parentId: '1', categoryId: '1' },
+          { name: 'conta filha 1.3', parentId: '1', categoryId: '1', id: '5' },
         ],
       },
       {
         name: 'conta mae 2', id: '2', categoryId: '3',
         children: [
-          { name: 'conta filha 2.1', parentId: '3', categoryId: '3' },
-          { name: 'conta filha 2.2', parentId: '3', categoryId: '3' },
+          { name: 'conta filha 2.1', parentId: '3', categoryId: '3', id: '7' },
+          { name: 'conta filha 2.2', parentId: '3', categoryId: '3', id: '8' },
         ],
       },
       {
         name: 'conta mae 3', id: '3', categoryId: '2',
         children: [
-          { name: 'conta filha 3.1', parentId: '3', categoryId: '2' },
-          { name: 'conta filha 3.2', parentId: '3', categoryId: '2' },
-          { name: 'conta filha 3.3', parentId: '3', categoryId: '2' },
-          { name: 'conta filha 3.3', parentId: '3', categoryId: '2' },
+          { name: 'conta filha 3.1', parentId: '3', categoryId: '2', id: '9' },
+          { name: 'conta filha 3.2', parentId: '3', categoryId: '2', id: '10' },
+          { name: 'conta filha 3.3', parentId: '3', categoryId: '2', id: '11' },
+          { name: 'conta filha 3.3', parentId: '3', categoryId: '2', id: '12' },
         ],
       },
       {
         name: 'conta mae 4', id: '4', categoryId: '1',
         children: [
-          { name: 'conta filha 4.1', parentId: '4', categoryId: '1' },
+          { name: 'conta filha 4.1', parentId: '4', categoryId: '1', id: '12' },
         ],
       },
     ];
