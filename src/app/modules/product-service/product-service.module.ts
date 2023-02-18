613b7a094c0fe60704c9c6e31cd2abd533ec3fc1
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-18 23:34:15 +0200

    created product service component

--- src/app/modules/product-service/product-service.module.ts
+++ src/app/modules/product-service/product-service.module.ts
@@ -2,10 +2,14 @@ import { ProductServiceRoutingModule } from './product-service-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductServiceListComponent } from './product-service-list/product-service-list.component';
+import { ProductServiceEditComponent } from './product-service-edit/product-service-edit.component';
+import { ProductServiceComponent } from './product-service.component';
 
 @NgModule({
   declarations: [
-    ProductServiceListComponent
+    ProductServiceListComponent,
+    ProductServiceEditComponent,
+    ProductServiceComponent
   ],
   imports: [
     CommonModule,
