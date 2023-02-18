5421e58d2c524d66399dc918cd328719b7374049
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-18 23:33:44 +0200

    added some product service routes

--- src/app/modules/product-service/product-service-routing.module.ts
+++ src/app/modules/product-service/product-service-routing.module.ts
@@ -1,3 +1,5 @@
+import { ProductServiceComponent } from './product-service.component';
+import { ProductServiceEditComponent } from './product-service-edit/product-service-edit.component';
 import { ProductServiceItemsResolver } from './shared/product-service.resolver';
 import { ProductServiceListComponent } from './product-service-list/product-service-list.component';
 import { NgModule } from '@angular/core';
@@ -5,10 +7,21 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', component: ProductServiceListComponent,
-    resolve: {
-      items: ProductServiceItemsResolver
-    }
+    path: '', component: ProductServiceComponent,
+    children: [
+      {
+        path: 'listar', component: ProductServiceListComponent,
+        resolve: {
+          items: ProductServiceItemsResolver
+        }
+      },
+      {
+        path: 'criar-producto', component: ProductServiceEditComponent,
+        resolve: {
+          items: ProductServiceItemsResolver
+        }
+      },
+    ],
   },
 ];
 
