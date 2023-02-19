f443ae726cbe68c97280993470c96201a4f3f1d8
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-19 08:58:07 +0200

    refactoring routerLink

--- src/app/skeleton/skeleton.component.ts
+++ src/app/skeleton/skeleton.component.ts
@@ -11,15 +11,16 @@ import { Component, OnInit } from '@angular/core';
         <ul>
           <li>contabilidade</li>
           <ul>
-            <li><a [routerLink]="['/inicio/contabilidade/plano-de-contas']">plano de contas</a></li>
-            <li><a [routerLink]="['/inicio/contabilidade/criar-conta']">criar conta</a></li>
+            <li><a routerLink="./contabilidade/plano-de-contas">plano de contas</a></li>
+            <li><a routerLink="./contabilidade/criar-conta">criar conta</a></li>
+            <li><a routerLink="./contabilidade/criar-taxa">criar taxa</a></li>
           </ul>
           
           <li>
-            <a [routerLink]="['/inicio/productos-servicos/listar']">productos e servicos</a>
+            <a routerLink="./productos-servicos/listar">productos e servicos</a>
             <ul>
-              <li><a [routerLink]="['/inicio/productos-servicos']">criar servico</a></li>
-                <li><a [routerLink]="['/inicio/productos-servicos/criar-producto']">criar producto</a></li>
+              <li><a routerLink="./productos-servicos">criar servico</a></li>
+                <li><a routerLink="./productos-servicos/criar">criar producto</a></li>
             </ul>
           </li>
         </ul>
