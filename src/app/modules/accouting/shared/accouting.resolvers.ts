820985479f3256d4fd0ef5ee35bedcbdbd5349a8
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-19 09:11:19 +0200

    created AccountingTaxesResolver

--- src/app/modules/accouting/shared/accouting.resolvers.ts
+++ src/app/modules/accouting/shared/accouting.resolvers.ts
@@ -1,8 +1,9 @@
+import { Tax } from './accouting.types';
 import { Account } from './../account/account.types';
 import { AccountingService } from './accounting.service';
 import { Category } from './../category/category.types';
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -45,5 +46,15 @@ export class AcountingAccoutingsResolver implements Resolve<Account[]> {
     console.log('normal');
     
     return this.accountingService.getAccounts();
+  }
+}
+
+export class AccountingTaxesResolver implements Resolve<Tax[]> {
+  constructor(
+    private accountingService: AccountingService
+  ) {}
+
+  resolve(): Observable<Tax[]> {
+    return this.accountingService.getTaxes();
   }
 }
