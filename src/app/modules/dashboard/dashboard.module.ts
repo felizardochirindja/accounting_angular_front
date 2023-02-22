d20855fa30ac03f714daac83e032966f8b532f23
felizardochirindja <felizardo.chirindja@gmail.com>
2023-02-22 12:11:17 +0200

    created dashboard module

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,17 @@
+import { HeaderComponent } from './../../ui/header/header.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    HeaderComponent,
+  ]
+})
+export class DashboardModule { }
