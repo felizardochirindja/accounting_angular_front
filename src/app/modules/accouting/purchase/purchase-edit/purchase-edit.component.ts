04d6c13c0ef44a4346e8aef4e5301e23bba27deb
felizardochirindja <felizardo.chirindja@gmail.com>
2023-04-17 23:44:39 +0200

    created basic form configuration to purchaase item

--- src/app/modules/accouting/purchase/purchase-edit/purchase-edit.component.ts
+++ src/app/modules/accouting/purchase/purchase-edit/purchase-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-edit',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   ]
 })
 export class PurchaseEditComponent {
+  purchaseFormGroup = new FormGroup({
+    name: new FormControl<string | null>(null),
+    purchasePrice: new FormControl<number | null>(null),
+    salePrice: new FormControl<number | null>(null),
+    quantity: new FormControl<number | null>(null),
+    category: new FormControl<string | null>(null),
+    supplier: new FormControl<string | null>(null),
+    storage: new FormControl<string | null>(null),
+  });
 
+  purchase(): void {
+    console.log(this.purchaseFormGroup.value);
+  }
 }
