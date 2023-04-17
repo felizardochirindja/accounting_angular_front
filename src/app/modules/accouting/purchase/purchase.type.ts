614bf8029538a6142fee81cddd530e3847539a9f
felizardochirindja <felizardo.chirindja@gmail.com>
2023-04-17 23:45:09 +0200

    created purchase.type.ts

---
+++ src/app/modules/accouting/purchase/purchase.type.ts
@@ -0,0 +1,10 @@
+export interface Purchase {
+    id?: string;
+    name?: string;
+    purchasePrice?: number;
+    salePrice?: number;
+    quantity?: number;
+    category?: string;
+    supplier?: string;
+    storage?: string;
+}
