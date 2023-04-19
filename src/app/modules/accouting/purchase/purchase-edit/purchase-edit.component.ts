c7a423ff6dfebf2c7745c58a82b598f98a10f915
felizardochirindja <felizardo.chirindja@gmail.com>
2023-04-19 13:35:09 +0200

    add error for required fields on edit purchase component

--- src/app/modules/accouting/purchase/purchase-edit/purchase-edit.component.ts
+++ src/app/modules/accouting/purchase/purchase-edit/purchase-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-edit',
@@ -9,13 +9,13 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class PurchaseEditComponent {
   purchaseFormGroup = new FormGroup({
-    name: new FormControl<string | null>(null),
-    purchasePrice: new FormControl<number | null>(null),
+    name: new FormControl<string | null>(null, Validators.required),
+    purchasePrice: new FormControl<number | null>(null, Validators.required),
     salePrice: new FormControl<number | null>(null),
-    quantity: new FormControl<number | null>(null),
-    category: new FormControl<string | null>(null),
-    supplier: new FormControl<string | null>(null),
-    storage: new FormControl<string | null>(null),
+    quantity: new FormControl<number | null>(null, Validators.required),
+    category: new FormControl<string | null>(null, Validators.required),
+    supplier: new FormControl<string | null>(null, Validators.required),
+    storage: new FormControl<string | null>(null, Validators.required),
   });
 
   supliers: string[] = [
