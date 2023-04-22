19c6f3de2116b814a4ee6b01595dce65e297d433
felizardochirindja <felizardo.chirindja@gmail.com>
2023-04-23 01:08:10 +0200

    imported http client module on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from 'src/environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AuthModule.forRoot(environment.auth0.config)
   ],
   bootstrap: [AppComponent]
