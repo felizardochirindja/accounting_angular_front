b9fb8ac2b29fc70ebae7bb10cdbae5d84a232f58
felizardochirindja <felizardo.chirindja@gmail.com>
2023-04-26 21:23:35 +0200

    ref: remove account on tax interface aand add description

--- src/app/modules/accouting/shared/accouting.types.ts
+++ src/app/modules/accouting/shared/accouting.types.ts
@@ -1,11 +1,10 @@
 import { Nuit } from 'src/app/core/valueObjects/Nuit.model';
-import { Account } from '../account/account.types';
 
 export interface Tax {
     id?: string;
     name?: string;
-    account?: Account;
     value?: number;
+    description?: string;
 }
 
 export interface Supplier {
