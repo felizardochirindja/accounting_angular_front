dd7d8d1f835732ad66ec0eea60fb7921ae2d3fbe
felizardochirindja <felizardo.chirindja@gmail.com>
2023-04-26 21:03:43 +0200

    ref: add id on client interface and removid supplier interface

--- src/app/core/client/client.types.ts
+++ src/app/core/client/client.types.ts
@@ -2,11 +2,10 @@ import { Nuit } from './../valueObjects/Nuit.model';
 import { Email } from './../valueObjects/Email.model';
 
 export interface Client {
+    id?: string;
     name?: string;
     nuit?: Nuit;
     local?: string;
     email?: Email;
     phone?: string;
 }
-
-export type Supplier = Client;
