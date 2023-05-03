7230d4be662846c55093afe1e11f8bbe9c9b9077
felizardochirindja <felizardo.chirindja@gmail.com>
2023-05-03 23:59:20 +0200

    feat: get products from AccountingService and implement logic to search products

--- src/app/modules/accouting/sale/sale-invoicing/sale-invoicing.component.ts
+++ src/app/modules/accouting/sale/sale-invoicing/sale-invoicing.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Product } from '../../shared/accouting.types';
+import { AccountingService } from './../../shared/accounting.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sale-invoicing',
   templateUrl: './sale-invoicing.component.html',
   styleUrls: ['./sale-invoicing.component.scss']
 })
-export class SaleInvoicingComponent {
+export class SaleInvoicingComponent implements OnInit {
+  private products: Product[] = [];
+  filteredProducts: Product[] = [];
 
+  searchProductField = new FormControl<string | null>(null);
+
+  constructor(
+    private accountingService: AccountingService
+  ) {}
+
+  ngOnInit(): void {
+    this.accountingService.products$.subscribe(products => {
+      this.products = this.filteredProducts = products;      
+    });
+
+    this.searchProductField.valueChanges.subscribe(searchTerm => {
+      this.filteredProducts = this.products.filter(
+        product => product.name?.toLocaleLowerCase().includes(searchTerm?.toLocaleLowerCase() as string)
+      );
+    });
+  }
+
+  addProduct(product: Product, quantity: number): void {
+    console.log(product, quantity);
+  }
 }
