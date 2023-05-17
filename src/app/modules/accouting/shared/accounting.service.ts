647e7aa4ce7dc0bbbf455c96bc983fab3211e2fe
felizardochirindja <felizardo.chirindja@gmail.com>
2023-05-17 15:23:12 +0200

    import rxjs of operator

--- src/app/modules/accouting/shared/accounting.service.ts
+++ src/app/modules/accouting/shared/accounting.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Category, Expense, ExpenseApiPayload, Product, ProductAPI, Storage, Supplier, Tax, TaxApiPayload } from './accouting.types';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map, switchMap, take } from 'rxjs';
+import { BehaviorSubject, Observable, map, of, switchMap, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Invoice, InvoiceAPI, Purchase, PurchaseAPI, PurchasePaymentMethod } from '../purchase/purchase.type';
 
