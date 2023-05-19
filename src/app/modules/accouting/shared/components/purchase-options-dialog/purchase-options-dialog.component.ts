73f3c2cc0165e2315ed8407f384d0fd7fd00793a
felizardochirindja <felizardo.chirindja@gmail.com>
2023-05-19 15:14:18 +0200

    Refactor PurchaseOptionsDialog to handle selected purchase types and close dialog

--- src/app/modules/accouting/shared/components/purchase-options-dialog/purchase-options-dialog.component.ts
+++ src/app/modules/accouting/shared/components/purchase-options-dialog/purchase-options-dialog.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-purchase-options-dialog',
   templateUrl: './purchase-options-dialog.component.html',
 })
 export class PurchaseOptionsDialogComponent {
-  selectedOption!: { name: string };
+  selectedPurchaseType!: string[];
+  dialogRef: MatDialogRef<PurchaseOptionsDialogComponent> = inject(MatDialogRef<PurchaseOptionsDialogComponent>);
 
-  purchaseOptions: { name: string }[] = [
-    { name: 'mercadorias' },
-    { name: 'consumiveis' },
-    { name: 'bens' },
+  purchaseTypes: string[] = [
+    'mercadorias',
+    'consumiveis',
+    'bens',
   ];
+
+  closeDialog() {
+    if (this.selectedPurchaseType.length === 0) {
+      throw new Error('No purchase type selected.'); 
+    }
+
+    this.dialogRef.close(this.selectedPurchaseType[0]);
+  }
 }
